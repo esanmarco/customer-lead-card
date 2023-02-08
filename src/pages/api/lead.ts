4cd766e68b6a0ea7fbad4c5cfa23b64faed2c5a7
Eddie SanMarco <esanmarco@linksquares.com>
2023-02-08 07:02:32 -0700

    removed unstable deprecated function

--- src/pages/api/lead.ts
+++ src/pages/api/lead.ts
@@ -1,6 +1,6 @@
 import { lead, PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { unstable_getServerSession } from "next-auth";
+import { getServerSession } from "next-auth";
 import { authOptions } from "./auth/[...nextauth]";
 
 const prisma = new PrismaClient();
@@ -9,7 +9,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await unstable_getServerSession(req, res, authOptions);
+  const session = await getServerSession(req, res, authOptions);
 
   if (!session?.user) {
     return res.status(401).json({ error: "Not authenticated" });
